3817ae0098463b8e4828d1ba7a23b51d414fa30d
naaskyy1002 <rrahmawatif.23@gmail.com>
2024-07-10 06:45:26 +0700

    comment

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,7 +17,6 @@ $routes->get('/data_siswa', 'Home::data_siswa');
 $routes->get('/daftar_pengunjung', 'Home::daftar_pengunjung');
 $routes->get('/jadwal_kunjungan', 'Home::jadwal_kunjungan');
 $routes->get('/profil_admin', 'Home::profil_admin');
-$routes->get('/bantuan', 'Home::bantuan');
 $routes->get('/masuk', 'Home::masuk');
 $routes->get('/welcome', 'Home::welcome');
 $routes->get('/tentang_kami', 'Home::tentang_kami');
