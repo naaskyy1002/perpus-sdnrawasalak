d0b28ab103a6e88bcfc12c28d86d2757a5ef8b91
snazizahin <sitinurazizah2203@gmail.com>
2024-07-18 09:05:44 +0700

    crud

--- app/Models/LoginModel.php
+++ app/Models/LoginModel.php
@@ -8,7 +8,7 @@ class LoginModel extends Model
     // Tabel untuk admin dan operator
     protected $tableAdmin = 'admin'; // Tabel admin
     protected $primaryKeyAdmin = 'nip';
-    protected $allowedFieldsAdmin = ['username', 'password', 'status', 'nama_lengkap'];
+    protected $allowedFieldsAdmin = ['username', 'password', 'level', 'nama_lengkap'];
 
     // Tabel untuk siswa
     protected $tableSiswa = 'siswa'; // Tabel siswa
